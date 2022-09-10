010e0dfe17a99a3fbaff62b0d993965f611ea55e
MelpCode <melp.code.01@gmail.com>
2022-09-09 20:14:25 -0500

    menus models added

--- django_menus_app/menus/models.py
+++ django_menus_app/menus/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Menus(models.Model):
+  starter = models.CharField(max_length=40)
+  mean = models.CharField(max_length=40)
+  dessert = models.CharField(max_length=40)
+  price = models.FloatField()
