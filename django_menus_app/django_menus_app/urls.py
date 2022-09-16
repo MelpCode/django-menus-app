ff2b9194c95bb1e7d7af30441d1bafb3b969631a
MelpCode <melp.code.01@gmail.com>
2022-09-15 22:22:51 -0500

    search_menu template updated

--- django_menus_app/django_menus_app/urls.py
+++ django_menus_app/django_menus_app/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('menus/new/',create_menu, name="create_menu"),
     path('menus/delete/<int:menu_id>',delete_menu, name="delete_menu"),
     path('menus/edit/<int:menu_id>', edit_menu, name="edit_menu"),
-
 ]
