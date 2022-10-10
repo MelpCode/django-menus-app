46149d17a66b22129fcbf40942bdcecf7d1806a1
MelpCode <melp.code.01@gmail.com>
2022-10-10 16:01:26 -0500

    fetch delete-menu added

--- django_menus_app/django_menus_app/static/js/main.js
+++ django_menus_app/django_menus_app/static/js/main.js
@@ -1,1 +1,20 @@
 console.log('Hello World')
+const deleteMenu = document.getElementById('delete-menu');
+
+deleteMenu.addEventListener('click',(e)=>{
+  e.preventDefault();
+  const id= deleteMenu.dataset.id;
+  const csrfToken = deleteMenu.dataset.csrftoken
+  if(confirm('Are you sure do you want to delete it?')){
+    fetch(`http://localhost:8000/menus/delete/${id}`,
+      {
+        method:'POST',
+        headers:{'X-CSRFToken':csrfToken}    
+      })
+    .then(res=>{
+      window.location.replace('http://localhost:8000/menus/')
+    })
+    .catch(err=>console.log(err))
+
+  }
+})
