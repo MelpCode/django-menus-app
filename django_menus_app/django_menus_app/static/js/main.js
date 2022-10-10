9e2ab9e3a4e152f58e98c3b6130ee182e7621f8c
MelpCode <melp.code.01@gmail.com>
2022-10-10 16:09:32 -0500

    validate click event

--- django_menus_app/django_menus_app/static/js/main.js
+++ django_menus_app/django_menus_app/static/js/main.js
@@ -1,20 +1,22 @@
-console.log('Hello World')
 const deleteMenu = document.getElementById('delete-menu');
 
-deleteMenu.addEventListener('click',(e)=>{
-  e.preventDefault();
-  const id= deleteMenu.dataset.id;
-  const csrfToken = deleteMenu.dataset.csrftoken
-  if(confirm('Are you sure do you want to delete this menu?')){
-    fetch(`http://localhost:8000/menus/delete/${id}`,
-      {
-        method:'POST',
-        headers:{'X-CSRFToken':csrfToken}    
+if (deleteMenu){
+  deleteMenu.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const id= deleteMenu.dataset.id;
+    const csrfToken = deleteMenu.dataset.csrftoken
+    const currentUrl = window.location.href
+    if(confirm('Are you sure do you want to delete this menu?')){
+      fetch(`${currentUrl}delete/${id}`,
+        {
+          method:'POST',
+          headers:{'X-CSRFToken':csrfToken}    
+        })
+      .then(res=>{
+        window.location.replace('http://localhost:8000/menus/')
       })
-    .then(res=>{
-      window.location.replace('http://localhost:8000/menus/')
-    })
-    .catch(err=>console.log(err))
+      .catch(err=>console.log(err))
+    }
+  })
+}
 
-  }
-})
